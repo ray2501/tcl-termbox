efe797891ddf6a9c8d9b4fe26a49b353426cc21a
Danilo Chang <ray2501@gmail.com>
2023-12-14 23:36:56 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded termbox 0.2 \
-    [list load [file join $dir libtermbox0.2.so] [string totitle termbox]]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded termbox 0.2 \
+	    [list load [file join $dir libtcl9termbox0.2.so] [string totitle termbox]]
+} else {
+    package ifneeded termbox 0.2 \
+	    [list load [file join $dir libtermbox0.2.so] [string totitle termbox]]
+}
